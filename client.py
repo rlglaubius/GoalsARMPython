d91fd84c5d7d662c5ea4c0d48352740c2700e2af
rlglaubius <rglaubius@avenirhealth.org>
2022-05-06 11:38:47 -0400

    Started adding support for reading inputs from an Excel workbook

--- client.py
+++ client.py
@@ -1,14 +1,31 @@
 import numpy as np
+import openpyxl as xlsx
 import os
 import sys
 import lib.Debug.GoalsARM as Goals
 
-def main():
-    first_year = 1970
-    final_year = 2030
+import src.goals_const as CONST
+
+def xlsx_load_config(cfgtab):
+    vals = [cell[0].value for cell in cfgtab['B2':'B8']]
+    keys = [cell[0].value for cell in cfgtab['D2':'D8']]
+    return dict(zip(keys,vals))
+
+def init_from_xlsx(xlsx_name):
+    wb = xlsx.load_workbook(filename=xlsx_name, read_only=True)
+    config = xlsx_load_config(wb[CONST.XLSX_TAB_CONFIG])
+
+    first_year = config[CONST.CFG_FIRST_YEAR]
+    final_year = config[CONST.CFG_FINAL_YEAR]
     model = Goals.Projection(first_year, final_year)
+
+    wb.close()
+    return model
+
+def main(xlsx_name):
+    model = init_from_xlsx(xlsx_name)
 
 if __name__ == "__main__":
     sys.stderr.write("Process %d" % (os.getpid()))
-    main()
-    
+    xlsx_name = "C:\\Proj\\Repositories\\GoalsARM\\tests\\example.xlsx"
+    main(xlsx_name)
---
+++ src/goals_const.py
@@ -0,0 +1,6 @@
+# Names of input workbook tabs
+XLSX_TAB_CONFIG = "Config"
+
+# Configuration tab tags
+CFG_FIRST_YEAR = "first.year"
+CFG_FINAL_YEAR = "final.year"
