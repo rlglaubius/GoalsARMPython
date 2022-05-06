191b4f71ad2815efa28340d1bbabbb4abb273312
rlglaubius <rglaubius@avenirhealth.org>
2022-05-06 18:02:23 -0400

    Added a factory method to hide some of the uglier details of wrapping numpy ndarray data in boost multi_array_ref

--- PyInterface.cpp
+++ PyInterface.cpp
@@ -1,6 +1,36 @@
 #include "PyInterface.h"
 #include <iostream>
 
+// Helper function. Returns a pointer to a two-dimensional boost::multi_array_ref
+// wrapper around the data stored in a numpy ndarray. NumType must be implicitly
+// castable from the arr dtype, otherwise this will probably cause some astonishing
+// errors. If you call this, you own the multi_array_ref pointer. Use it responsibly
+// and clean up after you are done with it.
+template<typename NumType>
+boost::multi_array_ref<NumType, 2>* generate2d(np::ndarray& arr) {
+	const int ext0 = arr.shape(0);
+	const int ext1 = arr.shape(1);
+	NumType* data_ptr = reinterpret_cast<NumType*>(arr.get_data());
+	boost::multi_array_ref<NumType, 2>* ptr;
+
+	if (arr.get_flags() & np::ndarray::C_CONTIGUOUS) {
+		ptr = new boost::multi_array_ref<NumType,2>(data_ptr, boost::extents[ext0][ext1], boost::c_storage_order());
+	} else if (arr.get_flags() & np::ndarray::F_CONTIGUOUS) {
+		ptr = new boost::multi_array_ref<NumType, 2>(data_ptr, boost::extents[ext0][ext1], boost::fortran_storage_order());
+	} else {
+		ptr = nullptr;
+	}
+
+	// TODO:
+	// Throw exception if
+	// * sizeof(arr.dtype()) != sizeof(NumType) [multi_array_ref datatype incompatible with arr dtype]
+	// ptr=nullptr case hit [mauled beyond recognition?]
+	// arr.get_nd() > 2 [array dimension is larger than multiarray dimension]
+	// other arr.get_flags() values that might cause issues?
+
+	return ptr;
+}
+
 PyInterface::PyInterface(const int year_start, const int year_final) {
 	proj = new DP::Projection(year_start, year_final);
 }
@@ -14,20 +44,7 @@ void PyInterface::initialize(const std::string& upd_filename) {
 }
 
 void PyInterface::init_pasfrs_from_5yr(np::ndarray& pasfrs5y) {
-	const int nrows(pasfrs5y.shape(0));
-	const int ncols(pasfrs5y.shape(1));
-	double* data_ptr = reinterpret_cast<double*>(pasfrs5y.get_data());
-	DP::year_age_ref_t* dat;
-
-	if (pasfrs5y.get_flags() & np::ndarray::C_CONTIGUOUS) {
-		dat = new DP::year_age_ref_t(data_ptr, boost::extents[nrows][ncols], boost::c_storage_order());
-	} else if (pasfrs5y.get_flags() & np::ndarray::F_CONTIGUOUS) {
-		dat = new DP::year_age_ref_t(data_ptr, boost::extents[nrows][ncols], boost::fortran_storage_order());
-	} else {
-		std::cerr << "Panic!" << std::endl;
-		dat = nullptr;
-	}
-
+	DP::year_age_ref_t* dat = generate2d<double>(pasfrs5y);
 	if (dat != NULL) {
 		proj->dat.init_pasfrs_from_5yr(*dat);
 		delete dat;
