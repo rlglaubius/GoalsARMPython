8eeedfae12f0a56ea12002c0330f47f10fd24cf1
rlglaubius <rglaubius@avenirhealth.org>
2022-05-13 17:50:42 -0400

    Built out more code for loading inputs into Python from Excel.

--- client.py
+++ client.py
@@ -6,6 +6,26 @@ import lib.Debug.GoalsARM as Goals
 
 import src.goals_const as CONST
 
+## TODO:
+## Config [partially done; TODO: net migration, direct incidence, direct CLHIV input]
+## MigrInputs [requires implementing Beers disaggregation here or C-side]
+## PopSizeInputs
+## DirectIncidenceInputs
+## PartnershipInputs
+## MixingMatrix
+## ContactInputs
+## EpiInputs
+## HIVDiseaseInputs
+## HIVFertilityInputs
+## ARTAdultInputs
+## MCInputs
+## DirectCLHIV
+
+# Return the contents of a range in an excel tab as a numpy array
+# The numpy array has float64 data type and "C" ordering
+def xlsx_load_range(tab, cell_first, cell_final):
+    return np.array([[cell.value for cell in row] for row in tab[cell_first:cell_final]], dtype=np.float64, order="C")
+
 # Return a dict() mapping configuration tags to their values
 # (e.g., x["first.year"] = 1970)
 def xlsx_load_config(tab_config):
@@ -14,8 +34,23 @@ def xlsx_load_config(tab_config):
     return dict(zip(keys,vals))
 
 def xlsx_load_pasfrs(tab_fert):
-    p = np.array([[cell.value for cell in row] for row in tab_fert['B2':'CD8']], dtype=np.float64, order="C")
+    p = xlsx_load_range(tab_fert, 'B2', 'CD8')
     return 0.01 * p.transpose()
+
+def xlsx_load_migr(tab_migr):
+    migr   = xlsx_load_range(tab_migr, 'B2',  'CD3')
+    dist_m = xlsx_load_range(tab_migr, 'B6',  'CD22')
+    dist_f = xlsx_load_range(tab_migr, 'B25', 'CD41')
+    return migr, dist_m, dist_f
+
+def xlsx_load_inci(tab_inci):
+    inci   = xlsx_load_range(tab_inci, 'B2',  'CD2')
+    sirr   = xlsx_load_range(tab_inci, 'B5',  'CD5')
+    airr_m = xlsx_load_range(tab_inci, 'B9',  'CD25')
+    airr_f = xlsx_load_range(tab_inci, 'B27', 'CD43')
+    rirr_m = xlsx_load_range(tab_inci, 'B47', 'CD53')
+    rirr_f = xlsx_load_range(tab_inci, 'B55', 'CD61')
+    return inci[0], sirr[0], airr_m, airr_f, rirr_m, rirr_f
 
 def init_from_xlsx(xlsx_name):
     wb = xlsx.load_workbook(filename=xlsx_name, read_only=True)
@@ -30,6 +65,14 @@ def init_from_xlsx(xlsx_name):
     if not config[CONST.CFG_USE_UPD_PASFRS]:
         pasfrs = xlsx_load_pasfrs(wb[CONST.XLSX_TAB_PASFRS])
         model.init_pasfrs_from_5yr(pasfrs)
+
+    if not config[CONST.CFG_USE_UPD_MIGR]:
+        migr_net, migr_dist_m, migr_dist_f = xlsx_load_migr(wb[CONST.XLSX_TAB_MIGR])
+        Warning("Net migration input from Excel is not supported yet")
+
+    if config[CONST.CFG_USE_DIRECT_INCI]:
+        inci, sirr, airr_m, airr_f, rirr_m, rirr_f = xlsx_load_inci(wb[CONST.XLSX_TAB_INCI])
+        Warning("Direct incidence input from Excel is not supported yet")
 
     wb.close()
     return model
--- src/goals_const.py
+++ src/goals_const.py
@@ -1,6 +1,8 @@
 # Names of input workbook tabs
 XLSX_TAB_CONFIG = "Config"
 XLSX_TAB_PASFRS = "FertilityInputs"
+XLSX_TAB_MIGR   = "MigrInputs"
+XLSX_TAB_INCI   = "DirectIncidenceInputs"
 
 # The Excel file specifies inputs for 1970-2050, the projection
 # uses a subset of these
@@ -8,7 +10,10 @@ XLSX_FIRST_YEAR = 1970
 XLSX_FINAL_YEAR = 2050
 
 # Configuration tab tags
-CFG_FIRST_YEAR = "first.year"
-CFG_FINAL_YEAR = "final.year"
-CFG_UPD_NAME = "upd.file"
-CFG_USE_UPD_PASFRS = "use.upd.pasfrs"
+CFG_FIRST_YEAR       = "first.year"
+CFG_FINAL_YEAR       = "final.year"
+CFG_UPD_NAME         = "upd.file"
+CFG_USE_UPD_PASFRS   = "use.upd.pasfrs"
+CFG_USE_UPD_MIGR     = "use.upd.migr"
+CFG_USE_DIRECT_INCI  = "use.direct.inci"
+CFG_USE_DIRECT_CLHIV = "use.direct.clhiv"
