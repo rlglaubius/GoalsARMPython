2a13e37225cb7091c16243566903bd398087bbb7
Rob Glaubius <rglaubius@avenirhealth.org>
2023-06-02 11:46:53 -0400

    The client now writes population size estimates to a CSV file. We added a dependency on pandas to simplify writing population outputs.

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 import sys
 import src.goals_const as CONST
 import time
@@ -12,7 +13,7 @@ def main(xlsx_name):
     t1 = time.time()
     model.init_from_xlsx(xlsx_name)
     t2 = time.time()
-    model.project(1980)
+    model.project(2030)
     t3 = time.time()
 
     sys.stdout.write("Construct\t%0.2fs\nInitialize\t%0.2fs\nProject\t%0.2fs\n" % (t1-t0, t2-t1, t3-t2))
@@ -20,9 +21,14 @@ def main(xlsx_name):
     results = Results(model)
     pop = results.bigpop()
     
+    pop_names = ['Year', 'Sex', 'Age']
+    pop_index = pd.MultiIndex.from_product([range(s) for s in pop.shape], names=pop_names)
+    pop_frame = pd.DataFrame({'Value' : pop.flatten()}, index=pop_index)['Value']
+    pop_frame.to_csv("bigpop.csv")
+
     pass
 
 if __name__ == "__main__":
     sys.stderr.write("Process %d\n" % (os.getpid()))
-    xlsx_name = "inputs\\example-unversioned-nohiv.xlsx"
+    xlsx_name = "inputs\\example-unversioned-nohiv-dbg.xlsx"
     main(xlsx_name)
